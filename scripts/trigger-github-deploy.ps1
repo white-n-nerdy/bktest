d147e0cd360bf26edaaec98abb64eba26fd17a2b
Miranda Del Fiacco <miranda_delfiacco@trimble.com>
2019-08-06 15:46:31 -0500

    bk

--- scripts/trigger-github-deploy.ps1
+++ scripts/trigger-github-deploy.ps1
@@ -1,4 +1,6 @@
 $releaseVersion = (& buildkite-agent meta-data get version)
+$hostname = (& buildkite-agent meta-data get version)
 
 Write-Output "hello world"
 Write-Output $releaseVersion
+Write-Output $hostname
