632d0b2e73a7251ca4ccb3dd3a62fc601da9cd41
Miranda Del Fiacco <miranda_delfiacco@trimble.com>
2019-08-06 22:49:45 -0500

    l

--- scripts/qa/app/trigger-github-deploy.ps1
+++
@@ -1,6 +0,0 @@
-$releaseVersion = (& buildkite-agent meta-data get release-version)
-$hostname = (& buildkite-agent meta-data get hostname)
-
-Write-Output 'qa'
-Write-Output $releaseVersion
-Write-Output $hostname
